354d261b9acb506ac310200f0e27b03e3f574ce8
Marat <roulinwhite@gmail.com>
2021-10-18 17:30:53 +0300

    Исправление ошибок, с переменными, оптимизация под мобильные устройства.

--- src/js/script.js
+++ src/js/script.js
@@ -109,7 +109,7 @@ $(document).ready(function(){
     });
     //smooth scroller script
     // Add smooth scrolling to all links
-     $("a").on('click', function(event) {
+     $("a[href=#up]").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -122,7 +122,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1000, function(){
+      }, 1200, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
